2a647c696355e5aae44f2c50b5ed2ff96826a664
Phantom <nV_M3@nV_M3-PC.(none)>
2013-03-27 10:45:24 -0700

    contact_form

--- contacto.php
+++ contacto.php
@@ -1,6 +1,39 @@
-<?php include 'header.php'; ?>
-
-
+﻿<?php 
+include 'header.php'; 
+$errors = array();
+$missing = array();
+// check if the form has been submitted
+if (isset($_POST['send'])) {
+  $to = 'clientes@prestamos-tijuana.com';
+  $subject = 'Nuevo Cliente';
+  
+  // list expected fields
+  $expected = array('name', 'tel', 'cel', 'nextel', 'email', 'cantidad', 'plazo', 'medio', 'nacionalidad', 'year', 'marca', 'modelo', 'transmision', 'millaje');
+  
+  // set required fields
+  $required = array('name', 'tel');
+  
+  // set default values for variables that might not exist
+  if (!isset($_POST['nacionalidad'])) {
+	$_POST['nacionalidad'] = '';
+  }
+  if (!isset($_POST['transmision'])) {
+	$_POST['transmision'] = '';
+  }
+  
+  // create additional headers
+  $headers = "From: Autopawn<clientes@prestamos-tijuana.com>\r\n";
+  $headers .= 'Content-Type: text/plain; charset=utf-8';
+  require('./processmail.inc.php');
+  
+  // TODO: Add a thank you page
+  if ($mailSent) {
+	header('Location: index.php');
+	exit;
+  }
+  
+}
+?>
 
 	<div class="row-fluid">
 		<div class="span12" id="mainContent">
@@ -15,21 +48,22 @@
 
             <div class="row-fluid">
             <div id="contactForm" class="span12">
-              <form>
+              <form id="contacto" method="post" action="">
                
-
               <div class="span4">
                  
                   <label>Nombre</label>
-                  <input type="text" name="name" required>
-                   <label>Telefono</label>
+                  <input type="text" name="name" required
+					<?php if ($missing || $errors) { 
+					echo 'value="' . htmlentities($name, ENT_COMPAT, 'UTF-8') . '"';
+					} ?>>
+                  <label>Telefono</label>
                   <input type="tel" name="tel" required>
-                   <label>Celular</label>
+                  <label>Celular</label>
                   <input type="tel" name="cel">
-                   <label>Nextel</label>
+                  <label>Nextel</label>
                   <input type="tel" name="nextel">
-                 
-                  
+				  
               </div>
 
               <div class="span4">
@@ -42,11 +76,11 @@
                   <input type="text" name="plazo">
                   <label>Porque medio se entero de nosotros</label>
                   <select name="medio">
-                    <option value="">Google</option>
-                    <option value="">Website de Seccion Amarilla</option>
-                    <option value="">Periodico El Mexicano</option>
-                    <option value="">Flyer/Volante</option>
-                    <option value="">Recomendacion</option>
+                    <option value="google">Google</option>
+                    <option value="seccion amarilla">Website de Seccion Amarilla</option>
+                    <option value="el mexicano">Periodico El Mexicano</option>
+                    <option value="volante">Flyer/Volante</option>
+                    <option value="recomendacion">Recomendación</option>
                   </select>
 
               </div>
@@ -54,43 +88,44 @@
               
                   <table class="table">
                     <tr>
-                      <td><input type="checkbox" name="" value=""> Nacional<br></td>
-                      <td><input type="checkbox" name="" value=""> Frontera<br></td>
-                      <td> <input type="checkbox" name="" value=""> Americano<br></td>
+                      <td><input type="radio" name="nacionalidad" value=""> Nacional<br></td>
+                      <td><input type="radio" name="nacionalidad" value=""> Frontera<br></td>
+                      <td><input type="radio" name="nacionalidad" value=""> Americano<br></td>
                     </tr>
                   </table>
                  
-                   <label>Ano</label>
-                   <input type="text" name="">
+                   <label>Año</label>
+                   <input type="text" name="year">
                    <label>Marca</label>
-                   <input type="text" name="">
+                   <input type="text" name="marca">
                    <label>Modelo</label>
-                   <input type="text" name="">
+                   <input type="text" name="modelo">
 
                    <table class="table">
                     <tr>
-                      <td><input type="checkbox" name="" value=""> Automatico<br></td>
-                      <td><input type="checkbox" name="" value=""> Estandar<br></td>
+                      <td><input type="radio" name="transmision" value="automatico"> Automatico<br></td>
+                      <td><input type="radio" name="transmision" value="estandar"> Estandar<br></td>
                     </tr>
                   </table>
 
-                  <label>Kilometraje/Milaje</label>
-                  <input type="text" name="">
+                  <label>Kilometraje/Millaje</label>
+                  <input type="text" name="millaje">
 
   
              </div><div class="clearfix"></div>
               <div class="row-fluid">
                 <div class="span4 offset8">
-                 <input type="submit" value="Enviar" class="btn-primary">
+                 <input type="submit" name ="send" value="Enviar" class="btn-primary">
                   <input type="reset" value="Limpiar" class="btn-primary">
               </div>
             </div>
          
-           </form>  
+           </form>
               
             <div class="clearfix"></div>
 
           
+		  
             </div><!-- mainCopy -->
         </div>
        
